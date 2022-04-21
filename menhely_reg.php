1d97dcb0135d802cce18eb3d8373442f3e01f362
release <vasember@msn.com>
2022-04-21 04:07:08 +0200

    fixed display issues of forms

--- menhely_reg.php
+++ menhely_reg.php
@@ -2,15 +2,15 @@
 	<form action='?A=processing' method='post' enctype='multipart/form-data'>
 			<input type="hidden" name="proc" value=3>
 		<ul class="form">
-			<li><label>Menhely név: <input type="text" name="menhelyname" size="26" maxlength="128" placeholder="Max 128 karakter" autofocus required></label></li>
-			<li><label>Email cím: <input type="email" name="email" size="26" maxlength="128" placeholder="valami@valami.hu" required></label></li>
-			<li><label>Jelszó: <input type="text" name="pass" size="26" minlength="8" placeholder="Minimum 8 karakter" required></label></li>
-			<li><label>Jelszó újra: <input type="text" name="pass2" size="26" required></label></li>
-			<li><label>Telefonszám: <input type="tel" name="phone" size="16" placeholder="06(30)123-4678" required></label></li>
-			<li><label>Rövid leírás: <textarea name="desc" cols="24" rows="5" maxlength="450" placeholder="Max 450 karakter"></textarea></label></li>
-			<li><label>Weboldal: <input type="text" name="website" maxlength="128" size="26" placeholder="A menhely weblapja ha van"></label></li>
-			<li><label>Város: <input type="text" name="city" maxlength="30" size="26" required></label></li>
-			<li><label>Cím: <input type="text" name="cim" maxlength="128" size="26" required></label></li>
+			<li><label>Menhely név: <input type="text" name="menhelyname" size="38" maxlength="128" placeholder="Max 128 karakter" autofocus required></label></li>
+			<li><label>Email cím: <input type="email" name="email" size="38" maxlength="128" placeholder="valami@valami.hu" required></label></li>
+			<li><label>Jelszó: <input type="text" name="pass" size="38" minlength="8" placeholder="Minimum 8 karakter" required></label></li>
+			<li><label>Jelszó újra: <input type="text" name="pass2" size="38" required></label></li>
+			<li><label>Telefonszám: <input type="tel" name="phone" size="38" placeholder="06(30)123-4678" required></label></li>
+			<li><label>Rövid leírás: <textarea name="desc" cols="38" rows="5" maxlength="450" placeholder="Max 450 karakter"></textarea></label></li>
+			<li><label>Weboldal: <input type="text" name="website" maxlength="128" size="38" placeholder="A menhely weblapja ha van"></label></li>
+			<li><label>Város: <input type="text" name="city" maxlength="30" size="38" required></label></li>
+			<li><label>Cím: <input type="text" name="cim" maxlength="128" size="38" required></label></li>
 			<li><label>Kép: <input type="file" name="pic" accept="image/jpeg" required></label></li>
 			<li class="center-button"><input class="button" type='submit' value='Regisztrálok'></li>
 			<li class="center-button">Már regisztráltál?</li>
--- upload_menhely.php
+++ upload_menhely.php
@@ -2,9 +2,9 @@
 	<form action='?A=processing' method='post' enctype='multipart/form-data'>
 		<input type="hidden" name="proc" value=5>
 		<ul class="form">
-			<li><label>Név: <input type="text" name="name" size="26" maxlength="32" placeholder="Max 32 karakter" autofocus required></label></li>
-			<li><label>Rövid leírás: <textarea name="desc" cols="24" rows="10" maxlength="450" placeholder="Max 450 karakter"></textarea></label></li>
-			<li><label>Kép: <input type='file' name='pic' size="26" accept="image/jpeg" required><br></label></li>
+			<li><label>Név: <input type="text" name="name" size="38" maxlength="32" placeholder="Max 32 karakter" autofocus required></label></li>
+			<li><label>Rövid leírás: <textarea name="desc" cols="38" rows="10" maxlength="450" placeholder="Max 450 karakter"></textarea></label></li>
+			<li><label>Kép: <input type='file' name='pic' size="38" accept="image/jpeg" required><br></label></li>
 			<li class="center-button"><input class="button" type='submit' value='Feltöltés'></li>
 		</ul>
 	</form>
